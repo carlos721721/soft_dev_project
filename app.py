1a634e0de71d9b6307e43cea5bd1560441f5c30e
carlos721721 <155716823+carlos721721@users.noreply.github.com>
2024-04-30 12:41:33 -0700

    Add Streamlit files and dataset

---
+++ app.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+# Read the dataset into a Pandas DataFrame
+@st.cache  # This decorator will cache the data, making it faster to load
+def load_data():
+    url = "https://practicum-content.s3.us-west-1.amazonaws.com/datasets/vehicles_us.csv"
+    df = pd.read_csv(url)
+    return df
+
+df = load_data()
+
+# Title/Header
+st.header("Vehicle Advertisement Dashboard")
+
+# Checkbox to toggle behavior
+show_histogram = st.checkbox("Show Histogram")
+
+# Plotly Express histogram
+if show_histogram:
+    st.write("## Distribution of Vehicle Prices")
+    fig_hist = px.histogram(df, x='price', title='Distribution of Vehicle Prices')
+    st.plotly_chart(fig_hist)
+
+# Checkbox to toggle behavior
+show_scatterplot = st.checkbox("Show Scatter Plot")
+
+# Plotly Express scatter plot
+if show_scatterplot:
+    st.write("## Vehicle Price vs. Mileage")
+    fig_scatter = px.scatter(df, x='odometer', y='price', title='Vehicle Price vs. Mileage')
+    st.plotly_chart(fig_scatter)
